99d5889fd28a3aa558f5e4a35764f67571f71f56
beccawilson <rebecca.c.wilson@gmail.com>
2014-05-07 15:05:51 +0100

    added colum of constant to create XtX
    Signed-off-by: beccawilson <rebecca.c.wilson@gmail.com>

--- weighted_poisson_glm/mb_btwa.R
+++ weighted_poisson_glm/mb_btwa.R
@@ -1,6 +1,6 @@
 #####
-#To generate B.A
-# mb_bt * a
+#To generate B.A MUST BIND CONST COLUMN 1 to a
+#
 #####
 ##########################################
 # these arguments to be read in from elsewhere e.g. fn_a_in <- commandArgs(7)
@@ -16,6 +16,9 @@ mb_btwa_out="/home/rw13742/Documents/datashield/testing/vertical_comms/data/mb_b
 
 a<-as.matrix(read.csv(a_in, header=TRUE))
 mb_btw<-as.matrix(read.csv(mb_btw_in, header=TRUE))
+
+const=rep(1, length(a[,1])) #add column of 1st to A.A matrix then don't need to do sums col/sumsrow
+a<-cbind(const, a)
 
 ##########################################
 #product at * a
