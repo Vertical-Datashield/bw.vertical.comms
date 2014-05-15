cc23283daa9ea2d7ec797af8d6175b9e40ed05af
beccawilson <rebecca.c.wilson@gmail.com>
2014-05-15 15:39:36 +0100

    updated masking a.b creating
    Signed-off-by: beccawilson <rebecca.c.wilson@gmail.com>

--- logistic_glm/ma_atb.R
+++ logistic_glm/ma_atb.R
@@ -1,47 +1,31 @@
 #####
 #To generate A.B
-# ma_at * b
+# 
 #####
 ##########################################
 # these arguments to be read in from elsewhere e.g. fn_a_in <- commandArgs(7)
 ##########################################
 
-fn_b_in="b.csv"
-fn_ma_at_in="ma_at.csv"
+b_in="/home/rw13742/Documents/datashield/testing/vertical_comms/data/b.csv"
+ma_at_in="/home/rw13742/Documents/datashield/testing/vertical_comms/data/ma_at.csv"
 
-fn_ma_atb_out="ma_atb.csv"
-
-loc_b_in="/home/rw13742/Documents/datashield/testing/vertical_comms/data/"
-loc_ma_at_in="/home/rw13742/Documents/datashield/testing/vertical_comms/data/"
-
-loc_ma_atb_out="/home/rw13742/Documents/datashield/testing/vertical_comms/data/"
-
-
-
-#########################################
-# set input and outputfiles 
-#########################################
-
-input_b=sprintf("%s%s", loc_b_in, fn_b_in)
-input_ma_at=sprintf("%s%s", loc_ma_at_in, fn_ma_at_in)
-output_ma_atb=sprintf("%s%s", loc_ma_atb_out, fn_ma_atb_out)
+ma_atb_out="/home/rw13742/Documents/datashield/testing/vertical_comms/data/ma_atb.csv"
 
 ###########################################
 #read files in
 ###########################################
 
-b<-as.matrix(read.csv(input_b, header=TRUE))
-ma_at<-as.matrix(read.csv(input_ma_at, header=TRUE))
+b<-as.matrix(read.csv(b_in, header=TRUE))
+ma_at<-as.matrix(read.csv(ma_at_in, header=TRUE))
 
 ###########################################
-#product at * a
-#product ma * ata
+#product atw * b to give a.b
 ###########################################
 ma_atb<-ma_at%*%b
 
 ###########################################
 # write AMa to file somewhere
 ###########################################
-write.table(ma_atb, row.names=FALSE, sep=",", file = output_ma_atb)
+write.table(ma_atb, row.names=FALSE, sep=",", file = ma_atb_out)
 
 
