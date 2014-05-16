eb8adf006194bf3ca3c3f221fe719fceb9c88af1
beccawilson <rebecca.c.wilson@gmail.com>
2014-05-16 14:56:36 +0100

    update bash script
    Signed-off-by: beccawilson <rebecca.c.wilson@gmail.com>

--- weighted_logistic_regression/weighted_logistic_regression.sh
+++ weighted_logistic_regression/weighted_logistic_regression.sh
@@ -1,8 +1,5 @@
 #! /usr/bin/env bash
-Rscript create_sums.R
-Rscript create_cloaking_vector.R
-Rscript get_linear_predictor.R
-Rscript client_gets_coeffs.R
-Rscript outcome_server_z.R
-Rscript client_calculates_coeffs.R
+1_get_variable_and_mask_a.R
+1_get_variable_and_mask_b.R
+1_make_pv0.R
 
