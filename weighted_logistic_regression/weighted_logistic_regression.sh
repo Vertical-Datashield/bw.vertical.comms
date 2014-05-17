951bb7f25689e324764b3d82b1a73be58fd3e16c
beccawilson <rebecca.c.wilson@gmail.com>
2014-05-17 14:23:39 +0100

    updated shell script
    Signed-off-by: beccawilson <rebecca.c.wilson@gmail.com>

--- weighted_logistic_regression/weighted_logistic_regression.sh
+++ weighted_logistic_regression/weighted_logistic_regression.sh
@@ -7,3 +7,5 @@ create_beta_table.R
 /lp_and_masking_vectors/3_pv2.R # pv1 + (beta * (masking vector*study variable))
 /lp_and_masking_vectors/4_p2.R # MV0 + MV2 
 /lp_and_masking_vectors/5_p1.R # P2 + MV1 = sum(masking vectors)
+/exp_gamma_p/1_last_study_tau.R #last study in source(study 2) creates t 
+/exp_gamma_p/2_frst_study_last_study_tau.R #product of 
